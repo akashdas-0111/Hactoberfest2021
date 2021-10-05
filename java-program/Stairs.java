c7018efe1ac76d10f0578fb392d638badf160e00
akashdas-0111 <61954560+akashdas-0111@users.noreply.github.com>
2021-10-05 17:32:13 +0530

    created Stairs.java
    Find total ways to reach the n’th stair from the bottom

---
+++ java-program/Stairs.java
@@ -0,0 +1,25 @@
+class Main
+{
+ 
+    public static int totalWays(int n)
+    {
+    
+        if (n < 0) {
+            return 0;
+        }
+ 
+       
+        if (n == 0) {
+            return 1;
+        }
+ 
+        return totalWays(n - 1) + totalWays(n - 2) + totalWays(n - 3);
+    }
+ 
+    public static void main(String[] args)
+    {
+        int n = 4;
+        System.out.printf("Total ways to reach the %d'th stair are %d",
+                            n, totalWays(n));
+    }
+}
